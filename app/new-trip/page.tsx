1b5ae6bc19d1502de6bfbd869a1163ce168ca1f4
Viva1981 <stuller.zsolt@gmail.com>
2025-11-14 20:07:07 +0100

    Fix supabase import (named export)

--- app/new-trip/page.tsx
+++ app/new-trip/page.tsx
@@ -2,7 +2,7 @@
 
 import { FormEvent, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import supabase from "../../lib/supabaseClient";
+import { supabase } from "../../lib/supabaseClient";
 
 type User = {
   id: string;
