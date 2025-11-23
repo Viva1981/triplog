fed559a53e7be7c8b05078d25c530a9a46aa5ded
Viva1981 <stuller.zsolt@gmail.com>
2025-11-23 21:36:17 +0100

    Fix import path: FileCard is in same folder, not /components

--- app/trip/[id]/PhotosSection.tsx
+++ app/trip/[id]/PhotosSection.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import type { TripFile } from "@/lib/trip/types";
-import FileCard from "./components/FileCard";
+import FileCard from "./FileCard";
 import { motion, PanInfo } from "framer-motion";
 
 type PhotosSectionProps = {
